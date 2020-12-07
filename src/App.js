fde3bf40a1061327733788ac79ecf102943976a8
otonye <otonyeamie@gmail.com>
2020-12-07 23:33:43 +0100

    home

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import data from './data.json';
+import Products from './components/products.component';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+  render() {
+    return (
     <div className="grid-container">
      <header>
        <a href='/'>React Shopping Cart</a>
      </header>
      <main>
-       Product List
+       <div className='content'>
+         <div className='main'>
+            <Products products={this.state.products}></Products>
+         </div>
+         <div className='sidebar'>
+           CartItems
+         </div>
+       </div>
      </main>
      <footer>
        All right is reserved
      </footer>
     </div>
   );
+  }
+  
 }
 
 export default App;
